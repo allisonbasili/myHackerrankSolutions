219802ce990bd2ca1a48205833625ce15b23157b
Allison Basili <allie@mac>
2021-07-14 20:58:40 -0400

    my correct answer

--- 009python-tuples/code/python-tuples.py
+++ 009python-tuples/code/python-tuples.py
@@ -4,5 +4,11 @@
 
 # Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
 
-
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    
+t = tuple(integer_list)
+    
+print(hash(t))
 
