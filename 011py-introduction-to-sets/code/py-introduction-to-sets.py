1a3048e4b07a391c65037292327061beb0ad2667
Allison Basili <allie@mac>
2021-07-17 15:10:33 -0400

    prompt

---
+++ 011py-introduction-to-sets/code/py-introduction-to-sets.py
@@ -0,0 +1,11 @@
+#Ms. Gabriel Williams is a botany professor at District College. 
+# ne day, she asked her student Mickey to compute the average 
+# of all the plants with distinct heights in her greenhouse.
+#Complete the average function in the editor below.
+
+## empty promt
+
+def average(array):
+    # your code goes here
+
+if __name__ == '__main__':
