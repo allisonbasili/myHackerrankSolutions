da62d58eea2ad1aa05e36e62450f9b0be424e45f
Allison Basili <allie@mac>
2021-07-17 16:07:08 -0400

    my solution

--- 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
+++ 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
@@ -1,7 +1,15 @@
 #  You are given  scores. Store them in a list and find the score of the runner-up.
 
-# empty prompt
+# my solution
 
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
+    arr = list(arr)
+    m = max(arr)
+    l = -9999999
+    for i in range(0,n):
+        if arr[i]<m and arr[i] > l:
+            l = arr[i]
+    
+    print(l)
