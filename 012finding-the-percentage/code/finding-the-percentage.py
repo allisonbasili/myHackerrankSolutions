59d569894c17a3d3339c5d24c62fc692cbfa9b3e
Allison Basili <allie@mac>
2021-07-17 15:30:33 -0400

    3rd commit

--- 012finding-the-percentage/code/finding-the-percentage.py
+++ 012finding-the-percentage/code/finding-the-percentage.py
@@ -1,7 +1,7 @@
 ##The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
 ##Print the average of the marks array for the student name provided, showing 2 places after the decimal.
 
-# answer!
+# 3rd commit
 n = int(input())
 student_marks = {}
 for i in range(n):
