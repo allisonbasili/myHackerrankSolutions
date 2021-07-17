58f54b00a171ac82cfddc9abd147ab9ba8a33a79
Allison Basili <allie@mac>
2021-07-17 15:34:52 -0400

    prompt

---
+++ 013whats-your-name/code/whats-your-anme.py
@@ -0,0 +1,13 @@
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#empy prompt
+
+def print_full_name(first, last):
+    # Write your code here
+
+if __name__ == '__main__':
