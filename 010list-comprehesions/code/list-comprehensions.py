137f43c819e9f812fd43bfebe50bd08d13baf24a
Allison Basili <allie@mac>
2021-07-17 14:59:07 -0400

    answer (correct)

--- 010list-comprehesions/code/list-comprehensions.py
+++ 010list-comprehesions/code/list-comprehensions.py
@@ -3,5 +3,6 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-
-    ## empy question
+## answer
+print ([[a,b,c] for a in range(x+1) for b in range(y+1) for c in range
+(z+1) if a + b + c != n ])
