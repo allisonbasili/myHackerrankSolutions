1f40063f410366b526b70d06abb3b659d4ef2ed4
Allison Basili <allie@mac>
2021-07-17 15:58:10 -0400

    empty prompt

---
+++ 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
@@ -0,0 +1,7 @@
+#  You are given  scores. Store them in a list and find the score of the runner-up.
+
+# empty prompt
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
