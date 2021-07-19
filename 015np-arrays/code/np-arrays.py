815d81fc88c7338613ec163b6ea199b868f86e30
Allison Basili <allie@mac>
2021-07-18 20:59:53 -0400

    empty prompt

---
+++ 015np-arrays/code/np-arrays.py
@@ -0,0 +1,13 @@
+# You are given a space separated list of numbers.
+#mYour task is to print a reversed NumPy array with the element type float.
+
+
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
